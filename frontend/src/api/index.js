91018f4877142f80b20b4e6f8c3aaedc03cfc57e
kramit624 <72662106+kramit624@users.noreply.github.com>
2026-03-08 20:12:49 +0530

    changes url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -8,7 +8,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://sales-dashboard-backend-hls4.onrender.com",
   timeout: 8000,
 });
 
